f4601634b2ad924a113c50c344a982e72fc9b958
rmbooth <54477979+rmbooth@users.noreply.github.com>
2020-01-10 15:23:35 -0500

    Update list.c

--- Part3/HW13Arithmetic1/list.c
+++ Part3/HW13Arithmetic1/list.c
@@ -23,6 +23,45 @@
 //    return true
 bool readList(char * filename, List * arithlist)
 {
+  if(arithlist == NULL)
+  {
+    return false;
+  }
+  FILE * fptr = fopen(filename,"r");
+  if(fptr == NULL)
+  {
+    return false;
+  }
+  char newArr [WORDLENGTH];
+  for(int a = 0;a< WORDLENGTH;a++)
+  {
+    newArr[a] = '\0';
+  }
+  int character = fgetc(fptr);
+  while(character != EOF)
+  {
+    int counter = 0;
+    while((character != '\n')&&(counter < WORDLENGTH - 1))
+    {
+      newArr[counter] = character;
+      counter++;
+      character = fgetc(fptr);
+    }
+    if(character != '\n')
+    {
+      deleteList(arithlist);
+      fclose(fptr);
+      return false;
+    }
+    newArr[counter] = character;
+    addNode(arithlist,newArr);
+    character = fgetc(fptr);
+    for(int a = 0;a< WORDLENGTH;a++)
+    {
+      newArr[a] = '\0';
+    }
+  }
+  fclose(fptr);
   return true;
 }
 #endif
@@ -30,14 +69,29 @@ bool readList(char * filename, List * arithlist)
 #ifdef TEST_DELETELIST
 // If arithlist is NULL, do nothing
 // release the memory of every node in the list
-// release the memory of the list 
+// release the memory of the list
 void deleteList(List * arithlist)
 {
+  if(arithlist == NULL)
+  {
+    return;
+  }
+  while(arithlist->head != NULL) //frees the head and redefines head down the list
+  {
+    ListNode * tempNode = arithlist->head;
+    arithlist->head = arithlist->head->next; //might cause a problem with NULL being last term
+    free(tempNode);
+  }
+
+  arithlist->head = NULL;
+  arithlist->tail=NULL;
+  free(arithlist);
+  arithlist=NULL;
 }
 #endif
 
 #ifdef TEST_ADDNODE
-// Input: 
+// Input:
 // arithlist stores the addresses of head and tail
 // If arithlist is NULL, do nothing
 // word is the word to be added
@@ -50,6 +104,30 @@ void deleteList(List * arithlist)
 // insert the ListNode to the list
 void addNode(List * arithlist, char * word)
 {
+  if(arithlist == NULL)
+  {
+    return;
+  }
+  if((arithlist->head==NULL)||(arithlist->tail==NULL))
+  {
+    ListNode * currNode = malloc(sizeof(ListNode));
+    arithlist->head=currNode;
+    arithlist->tail=currNode;
+    currNode->prev=NULL;
+    currNode->next=NULL;
+    memcpy(currNode->word, word, sizeof(char)*WORDLENGTH);
+  }
+  else
+  {
+    ListNode * currNode = arithlist->tail;
+    ListNode * newNode = malloc(sizeof(ListNode));
+    currNode->next=newNode;
+    newNode->prev=currNode;
+    newNode->next=NULL;
+    arithlist->tail = newNode;
+    memcpy(newNode->word, word, sizeof(char)*WORDLENGTH);
+  }
+
 }
 #endif
 
@@ -60,7 +138,7 @@ void addNode(List * arithlist, char * word)
 // If the list is empty (head and tail are NULL), return false
 // ln is the node to be deleted
 // If ln is not in the list, return false
-// 
+//
 // Output:
 // arithlist stores the addresses of head and tail
 //   after ln is deleted
@@ -69,7 +147,60 @@ void addNode(List * arithlist, char * word)
 // Be careful about delete the first or the last node
 bool deleteNode(List * arithlist, ListNode * ln)
 {
-  return true;
+  if (arithlist == NULL)
+  {
+    return false;
+  }
+  if ((arithlist->head == NULL)||(arithlist->tail == NULL))
+  {
+    return false;
+  }
+  ListNode * currNode = arithlist -> head;
+  while ((currNode->next != NULL)&&(currNode != ln))
+  {
+    currNode = currNode ->next;
+  }
+  if (currNode == ln) //if the node to delete is present in the list
+  {
+    if(currNode == arithlist-> head) //if the first is to be deleted
+    {
+      if(arithlist->head == arithlist->tail) //if there is one element
+      {
+        free(currNode);
+        arithlist->head = NULL;
+        arithlist->tail = NULL;
+        //free(arithlist);
+        return true;
+      }
+      arithlist -> head = arithlist ->head-> next; //redefining head
+      free(currNode);
+      currNode = arithlist->head;
+      currNode->prev = NULL;
+      return true;
+    }
+    if(currNode == arithlist-> tail) //if the last is to be deleted
+    {
+      arithlist -> tail = arithlist ->tail-> prev; //redefining head
+      free(currNode);
+      currNode = arithlist->tail;
+      currNode->next = NULL;
+      return true;
+    }
+    else
+    {
+      currNode->prev->next = currNode->next;  //redefining pointers for List
+      currNode->next->prev = currNode->prev;
+
+      free(currNode); //deleting node
+      currNode = NULL;
+      return true;
+    }
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+  return false;
 }
 #endif
-
