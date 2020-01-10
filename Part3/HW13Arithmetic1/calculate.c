73af0885a3b2ac0391ccedb80fb3552035bc7a62
rmbooth <54477979+rmbooth@users.noreply.github.com>
2020-01-10 15:24:26 -0500

    Update calculate.c

--- Part3/HW13Arithmetic1/calculate.c
+++ Part3/HW13Arithmetic1/calculate.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "calculate.h"
 
 // DO NOT MODIFY FROM HERE --->>>
@@ -20,7 +21,8 @@ int isOperator(char * word)
   int numop = sizeof(Operations) / sizeof(int);
   for (ind = 0; ind < numop; ind ++)
     {
-      if (strchr(word, Operations[ind]) != NULL)
+      char *loc = strchr(word,Operations[ind]);
+      if (loc != NULL && !isdigit(loc[1]))
 	{
 	  return ind;
 	}
@@ -61,9 +63,80 @@ bool calculate(List * arithlist)
   //     the list should have exactly one node left. If this is not
   //     true, return false
   // If the input is valud, return true
+  ListNode * currNode = arithlist->head;
+  int numbers = 0;
+  int operators = 0;
+  while(currNode!=NULL)
+  {
+    if (isOperator(currNode->word) == -1)
+    {
+      numbers++;
+    }
+    else
+    {
+      operators++;
+    }
+    currNode = currNode->next;
+  }
+  if ((operators == 0)&&(numbers ==1))
+  {
+    return true;
+  }
+  if ((operators < 1)&&(numbers < 2))
+  {
+    return false;
+  }
+  if (numbers - operators != 1)
+  {
+    return false;
+  }
+  currNode = arithlist->head;
+  List * stack = malloc(sizeof(List));
+  stack->head = NULL;
+  stack->tail = NULL;
 
+  while((stack->head != stack -> tail)||(stack->head == NULL))
+  {
+    while(isOperator(currNode->word) == -1 )
+    {
+      addNode(stack,currNode->word);
+      currNode = currNode->next;
+    }
+    char *trash;
+    int tail = strtol(stack->tail->word,&trash,10);
+    int tailPrev = strtol(stack->tail->prev->word,&trash,10);
+    int var;
+    if(isOperator(currNode->word) == 0)
+    {
+      var = tailPrev + tail;
+    }
+    if(isOperator(currNode->word) == 1)
+    {
+      var = tailPrev - tail;
+    }
+    if(isOperator(currNode->word) == 2)
+    {
+      var = tailPrev * tail;
+    }
+    deleteNode(stack,stack->tail);
+    deleteNode(stack,stack->tail);
+    char temp[WORDLENGTH];
+    sprintf (temp,"%d\n",var); //convert value to string
+    addNode(stack,temp);
+    if(currNode->next!= NULL)
+    {
+      currNode = currNode->next;
+    }
+  }
+  ListNode * p = arithlist->head;
+  ListNode * q = arithlist->tail;
 
-  
+  arithlist->head = stack->head;
+  arithlist->tail = stack->tail;
+
+  stack->head = p;
+  stack->tail=q;
+  deleteList(stack);
   // if more than one node left, return false
 
   // if the remaining node is an operator, return false
