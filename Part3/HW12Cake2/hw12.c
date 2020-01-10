eff76495af162d0f0b6fa30c31a83d8765f5f422
rmbooth <54477979+rmbooth@users.noreply.github.com>
2020-01-10 15:22:00 -0500

    Update hw12.c

--- Part3/HW12Cake2/hw12.c
+++ Part3/HW12Cake2/hw12.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 #include "hw12.h"
 
@@ -31,6 +31,22 @@ void printListNode(ListNode * head)
 // return the head of the linked listn
 ListNode * createList(int valn)
 {
+  ListNode * head = malloc(sizeof(ListNode));
+  ListNode * currNode = head;
+  //currNode ->next =NULL;
+  int count = 0;
+  currNode -> value = count;
+  count++;
+  while (count < valn)
+  {
+    currNode -> next = malloc(sizeof(ListNode));
+    currNode = currNode -> next;
+    currNode -> value = count;
+    count++;
+    currNode->next = NULL;
+
+  }
+  return head;
 }
 #endif
 
@@ -45,12 +61,45 @@ ListNode * createList(int valn)
 // print the values of the nodes to be deleted
 void eliminate(ListNode * head, int valk)
 {
-#ifdef DEBUG
-  // this #ifdef ... #endif should be inside the condition *BEFORE* a
-  // node' value is printed and it is deleted
-  ListNode * todelete = p;
-  printListNode (todelete); 
-#endif
+  ListNode * currNode = head;
+  while (head != NULL)
+  {
+    for (int i = 1; i < valk; i++)
+    {
+      if (currNode -> next == NULL)
+      {
+        currNode = head;
+      }
+      else
+      {
+        currNode = currNode -> next;
+      }
+    }
+    #ifdef DEBUG
+      // this #ifdef ... #endif should be inside the condition *BEFORE* a
+      // node' value is printed and it is deleted
+      if ((head!= NULL) && (head->next != NULL))
+      {
+        ListNode * todelete = currNode;
+        printListNode (todelete);
+      }
+    #endif
+    printf("%d\n",currNode->value);
+    ListNode * temp = head;
+    if (currNode->next!=NULL)
+    {
+      temp = currNode -> next;
+    }
+    head = deleteNode(head,currNode);
+    if(head == NULL)
+    {
+      currNode = NULL;
+    }
+    else
+    {
+      currNode = temp;
+    }
+  }
 }
 #endif
 
@@ -71,6 +120,63 @@ void eliminate(ListNode * head, int valk)
 // the head). If this occurs, return the second node of the list.
 ListNode * deleteNode(ListNode * head, ListNode * todelete)
 {
+  if(head == NULL) //if head is null
+  {
+    return NULL;
+  }
+  if(todelete == NULL) //if todelete is null
+  {
+    return head;
+  }
+
+  if (head->value == todelete -> value)
+  {
+    if(head->next != NULL)
+    {
+      ListNode * temp = head -> next;
+      free (head);
+      head = temp;
+      return head;
+    }
+    else
+    {
+      free(head);
+      return NULL;
+    }
+  }
+
+  if(head->next != NULL) //if the head next is null
+  {
+    ListNode * currNode = head;
+    while(currNode->next->value != todelete->value)
+    {
+      if(currNode->next->next == NULL) //checking the next next term for NULL
+      {
+        return head; //return the head value
+      }
+      currNode = currNode -> next;
+    }
+    if(currNode->next->next==NULL) //checking the next next currNode for NULL
+    {
+      free(currNode->next);
+      currNode -> next = NULL;
+      return head;
+    }
+    ListNode * temp = currNode->next->next;
+    free(currNode->next);
+    currNode-> next = temp;
+    return head;
+  }
+  else
+  {
+    if (head-> value == todelete->value) //if head value is equal to todelete value
+    {
+      free(head);
+      head = NULL;
+      return NULL;
+    }
+    return head;
+  }
 }
 #endif
 
