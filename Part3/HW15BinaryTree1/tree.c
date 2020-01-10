789ddffdb576b932294e85bb8b5cd22b79f769ec
rmbooth <54477979+rmbooth@users.noreply.github.com>
2020-01-10 15:26:16 -0500

    Update tree.c

--- Part3/HW15BinaryTree1/tree.c
+++ Part3/HW15BinaryTree1/tree.c
@@ -63,5 +63,102 @@ void preOrder(Tree * tr, char * filename)
 
 Tree * buildTree(int * inArray, int * postArray, int size)
 {
+   if (size == 0)
+   {
+     return NULL;
+   }
+   if (inArray == NULL)
+   {
+     return NULL;
+   }
+   if (postArray == NULL)
+   {
+     return NULL;
+   }
+
+   Tree * head = malloc (sizeof(Tree));
+   if (head == NULL)
+   {
+     return NULL;
+   }
+   head->root = NULL;
+
+   TreeNode * treeArr = malloc(sizeof(TreeNode));
+   if (treeArr == NULL)
+   {
+     return NULL;
+   }
+   int newSize;
+   head -> root = treeArr;
+   treeArr -> value  = postArray[size - 1];
+   treeArr -> left = NULL;
+   treeArr -> right = NULL;
+
+   if (size == 1)
+   {
+     return head;
+   }
+   for(int i = 0; i < size; i++)
+   {
+     if (inArray[i] == treeArr -> value)
+     {
+       newSize = i;
+     }
+   }
+
+   int * newPostArry = malloc(sizeof(int)*newSize);
+   int countLeft = 0;
+   for (int i = 0; i < size; i++)
+   {
+     for (int a = 0; a < newSize;a++)
+     {
+       if (inArray[a] == postArray[i])
+       {
+         newPostArry[countLeft] = postArray[i];
+         countLeft++;
+       }
+     }
+   }
+   Tree * left = buildTree(inArray,newPostArry,newSize);
+
+   if (left == NULL)
+   {
+     head->root -> left = NULL;
+   }
+   else
+   {
+     head -> root ->left= left->root;
+     free(left);
+   }
+   free(newPostArry);
+
+   int * newRightPostArry = malloc(sizeof(int)*(size-newSize-1));
+   int countRight = 0;
+   for (int i = 0; i < size; i++)
+   {
+     for (int a = newSize+1; a < size;a++)
+     {
+       if (inArray[a] == postArray[i])
+       {
+         newRightPostArry[countRight] = postArray[i];
+         countRight++;
+       }
+     }
+   }
+   Tree * right = buildTree(inArray + newSize + 1,newRightPostArry, size - newSize-1);
+   if (right == NULL)
+   {
+     head->root -> right = NULL;
+   }
+   else
+   {
+     head -> root ->right= right->root;
+     free(right);
+   }
+   free(newRightPostArry);
+
+   return head;
+
+
 }
 #endif
